e4e12e166e700372f8b7e2fb50d643d14a43cc48
HP_Gayatri <vishuyadav2203@gmail.com>
2026-01-10 21:53:27 +0530

    2nd commit local mongo atlas error

--- project_2/settings.py
+++ project_2/settings.py
@@ -14,12 +14,12 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
-# Load environment variables from .env file
-load_dotenv()
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Load environment variables from .env file
+load_dotenv(BASE_DIR / ".env.local")
 
 
 # Quick-start development settings - unsuitable for production
